5b7f16d8988716cf455f55de823939a4987b8593
nengtamala16-dotcom <nengtamala73@gmail.com>
2026-09-11 09:49:28 +0700

    feat: penambahan fitur tambah

---
+++ halaman_dasboard/data_peminjaman/tambah.php
@@ -0,0 +1,40 @@
+<?php
+include "koneksi.php";
+
+if (isset($_POST['submit'])) {
+    $judul_buku = $_POST['judul_buku'];
+    $pengarang = $_POST['pengarang'];
+    $penerbit = $_POST['penerbit'];
+    $tahun_terbit = $_POST['tahun_terbit'];
+    $sinopsis = $_POST['sinopsis'];
+    $stok = $_POST['stok'];
+
+    $query = "INSERT INTO pinjam
+              (judul_buku,pengarang,penerbit,tahun_terbit,sinopsis,stok) 
+              VALUES
+               ('$judul_buku','$pengarang','$penerbit','$tahun_terbit','$sinopsis','$stok')";
+
+    mysqli_query($koneksi, $query);
+    header("location:tampil.php");
+}
+?>
+
+<html>
+<head>
+    <title>Pinjam</title>
+</head>
+<body>
+
+<form action="" method="POST">
+    <input type="text" name="judul_buku" placeholder="Nama Anime">
+    <input type="text" name="pengarang" placeholder="Karakter Anime">
+    <input type="text" name="penerbit" placeholder="Jenis Kelamin">
+    <input type="text" name="tahun_terbit" placeholder="Jenis Kelamin">
+    <input type="text" name="sinopsis" placeholder="Jenis Kelamin">
+    <textarea type="text" name="stok" placeholder="Jenis Kelamin"></textarea>
+
+    <button type="submit" name="submit">Simpan</button>
+</form>
+
+</body>
+</html>
